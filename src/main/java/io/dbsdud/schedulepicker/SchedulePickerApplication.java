a7f108a38a1760ccec891103d7984e078b17bd1d
dbsdud <duddl425@gmail.com>
2022-11-12 09:49:13 +0900

    first commit

---
+++ src/main/java/io/dbsdud/schedulepicker/SchedulePickerApplication.java
@@ -0,0 +1,13 @@
+package io.dbsdud.schedulepicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulePickerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulePickerApplication.class, args);
+    }
+
+}
